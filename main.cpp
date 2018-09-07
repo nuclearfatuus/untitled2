9518691e9ab05df723f60bace0625c6b50fe280b
nuclearfatuus <nuclearfatuus@gmail.com>
2018-09-07 17:55:23 +0300

    char

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 int main() {
     int a = 7;
+    char b = 92;
+
+    std::cout << char(b*2) << std::endl;
+    std::cout << char(b/2) << std::endl;
+    std::cout << char(b+2) << std::endl;
+    std::cout << char(b-2) << std::endl;
     std::cout << a*3 << std::endl;
     std::cout << a/3 << std::endl;
     std::cout << a +3 << std::endl;
