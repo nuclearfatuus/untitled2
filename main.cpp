9af46121609cce72d2abb97cce3a7d2a70d85302
nuclearfatuus <nuclearfatuus@gmail.com>
2018-09-07 17:36:22 +0300

    calcul

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a = 7;
+    std::cout << a*3 << std::endl;
+    std::cout << a/3 << std::endl;
+    std::cout << a +3 << std::endl;
+    std::cout << a -3 << std::endl;
+    return 0;
+}
